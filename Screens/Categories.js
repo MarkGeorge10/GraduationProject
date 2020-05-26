ef6e940439bc36cc6d0faf8b998a628583696f68
unknown <you@example.com>
2020-05-26 09:51:56 -0700

    Productdetails

---
+++ Screens/Categories.js
@@ -0,0 +1,70 @@
+import React , {Component} from 'react';
+import {View, Text,StyleSheet,TouchableOpacity,Button} from 'react-native';
+import {Card} from 'react-native-paper'
+class  Categories extends Component{
+
+    render(){
+
+      return (
+        
+            <View style={styles.container}>
+                <View style={styles.articleContainer}>
+                    <Text style={styles.heading}>Welcome to Products Page</Text>
+                    <Button
+                        title=" Go to Products"
+                        onPress={() => this.props.navigation.navigate('Products')}
+                         />
+                  
+    
+                    <TouchableOpacity style={{padding:20}}>
+                        <Text style={{color:'#189cfc'}}>Logout</Text>
+                    </TouchableOpacity>
+    
+                </View>
+            </View>
+   
+    );
+
+    }
+  }
+
+
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center'
+
+
+    },
+    heading:{
+        fontSize:22,
+        color:'black',
+        padding:10,
+        marginBottom:10,
+    },
+    content:{
+        marginTop:10,
+        fontSize:19
+    },
+    articleContainer:{
+        padding:10,
+        borderBottomWidth: 5,
+        borderBottomColor:'rgba(255,255,255,.7)',
+
+    },
+    mycard:{
+
+margin:10,
+
+    },
+
+ cardview:{
+flexDirection:'row',
+
+
+ }   
+});
+
+export default Categories;
